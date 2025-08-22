79810b8a3a7fab64e701e2e673908a57c090257f
sammyne <xiangmin-li@qq.com>
2025-08-22 01:32:51 +0000

    feat(03-building-blocks-of-ai-agents): impl custom-tools

---
+++ 03-building-blocks-of-ai-agents/README.md
@@ -0,0 +1,5 @@
+# 03. Building blocks of AI Agents
+
+## 温馨提示
+- CrewAI 底层依赖 LiteLLM，对于 LiteLLM 非官方支持、但提供兼容 OpenAI 的访问接口的模型提供方（例如硅基流动），，需要为 `model` 参
+  数添加 `openai/` 前缀。相关文档参见 https://docs.litellm.ai/docs/providers/openai_compatible
---
+++ 03-building-blocks-of-ai-agents/src/01_custom_tools.py
@@ -0,0 +1,94 @@
+import os
+from typing import Type
+
+from crewai.tools import BaseTool
+from crewai import Agent, Crew, Process, Task
+from pydantic import BaseModel, Field
+
+from config import Config
+
+c = Config()
+llm = c.new_crewai_llm()
+
+
+class CurrencyConverterInput(BaseModel):
+    """
+    Input schema for CurrencyConverterTool.
+    """
+
+    amount: float = Field(..., description="The amount to convert")
+    from_currency: str = Field(
+        ..., description="The source currency code (e.g., 'USD')"
+    )
+    to_currency: str = Field(..., description="The target currency code (e.g., 'EUR')")
+
+
+class CurrencyConverterTool(BaseTool):
+    name: str = "Currency Converter Tool"
+    description: str = "Converts an amount from one currency to another"
+    args_schema: Type[BaseModel] = CurrencyConverterInput
+    api_key: str | None = os.getenv("EXCHANGE_RATE_API_KEY")
+
+    def _run(self, amount: float, from_currency: str, to_currency: str) -> str:
+        """
+        Converts an amount from one currency to another.
+        """
+        import requests
+
+        url = (
+            f"https://v6.exchangerate-api.com/v6/{self.api_key}/latest/{from_currency}"
+        )
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception(f"Error: {response.status_code}")
+
+        data = response.json()
+
+        if (
+            "conversion_rates" not in data
+            or to_currency not in data["conversion_rates"]
+        ):
+            raise Exception(f"Invalid currency code: {to_currency}")
+
+        rate = data["conversion_rates"][to_currency]
+        converted_amount = amount * rate
+        return (
+            f"{amount} {from_currency} is equal to {converted_amount:.2f} {to_currency}"
+        )
+
+
+currency_analyst = Agent(
+    role="Currency Analyst",
+    goal="Provide real-time currency conversions and finacial analysis",
+    backstory=(
+        "You are a finace expert with deep knowledge of global exchange rates."
+        "You help users with currency conversions and finacial decision-making."
+    ),
+    tools=[CurrencyConverterTool()],
+    llm=llm,
+    verbose=True,
+)
+
+currency_conversion_task = Task(
+    description=(
+        "Convert {amount} {from_currency} to {to_currency} "
+        "using real-time exchange rates."
+        "Provide the equivalent amount and explain any relevant financial context."
+    ),
+    expected_output=(
+        "A detailed response including the converted amount and financial insights."
+    ),
+    agent=currency_analyst,
+)
+
+crew = Crew(
+    agents=[currency_analyst],
+    tasks=[currency_conversion_task],
+    process=Process.sequential,
+)
+
+response = crew.kickoff(
+    inputs={"amount": 100, "from_currency": "USD", "to_currency": "EUR"}
+)
+print(response)
